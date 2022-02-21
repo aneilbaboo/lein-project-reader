459b0582cfbea5514d76cb2745e92c5d39c27654
Aneil Mallavarapu <aneilmallavar@gmail.com>
2022-02-21 07:47:27 -0800

    Deploy to clojars

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject lein-project-reader "0.0.1"
+(defproject net.clojars.aneilm/lein-project-reader "0.0.1"
   :description "Provides access to the Leiningen project definition"
   :url "https://github.com/aneilbaboo/lein-project-reader"
   :license {:name "MIT"
-            :url "https://opensource.org/licenses/MIT"}
+            :url  "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repositories [["releases" {:url   "https://repo.clojars.org"
+                              :creds :gpg}]]
   :repl-options {:init-ns lein-project-reader.core})
